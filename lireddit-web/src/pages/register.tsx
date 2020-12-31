3d03248e0ff3521e0de23d0a863e3419740c27b5
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-01 01:14:33 +0530

    feat(client): add register page layout

---
+++ lireddit-web/src/pages/register.tsx
@@ -0,0 +1,48 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import React from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper varient="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values, actions) => {
+          console.log(values, actions);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              label="Username"
+              name="username"
+              placeholder="username"
+            />
+            <Box mt={4}>
+              <InputField
+                label="Password"
+                name="password"
+                placeholder="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
