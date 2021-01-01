7a1f68d3c83006754acd8556a82eed4782fc5285
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-01 21:47:18 +0530

    feat(client): add graphql code generation
    - generates files with types to our graphql query/mutation
    - provides a hook, that we can use instead of useMutation provided by urql, gives us an abstraction

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,35 +1,22 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import React from "react";
-import { useMutation } from "urql";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
-const REGISTER_MUT = `
-mutation Register($username: String!, $password: String!) {
-  register(options: { username: $username, password: $password }) {
-		errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}
-`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [{}, register] = useMutation(REGISTER_MUT);
+  const [{}, register] = useRegisterMutation();
+
   return (
     <Wrapper varient="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          return register(values);
+        onSubmit={async (values) => {
+          const response = await register(values);
+          return;
         }}
       >
         {({ isSubmitting }) => (
