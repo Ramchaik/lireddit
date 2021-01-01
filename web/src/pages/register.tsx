9be2c8e1a9e29e3fc3029e860cf3bd7bd293d34c
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-01 21:41:23 +0530

    feat(client): add urql client to send req using useMutation hook

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,18 +1,35 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import React from "react";
+import { useMutation } from "urql";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+		errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [{}, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper varient="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values, actions) => {
-          console.log(values, actions);
+        onSubmit={(values) => {
+          return register(values);
         }}
       >
         {({ isSubmitting }) => (
