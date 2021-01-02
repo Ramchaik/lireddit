6085208aa40d6d3ca7db34b9349e8f61e6a74384
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-02 15:07:25 +0530

    feat(web): setup SSR using next with graphql

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,27 @@
+import { withUrqlClient } from "next-urql";
 import { NavBar } from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello world</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-export default Index;
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        <ul>
+          {data.posts.map((p) => (
+            <li key={p.id}>{p.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
