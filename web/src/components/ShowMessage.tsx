86a868befbcac9076a248c88a31ea3c45a51e75c
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-07 21:06:46 +0530

    refactor(web): update component to show generic msg

--- web/src/components/ShowMessage.tsx
+++ web/src/components/ShowMessage.tsx
@@ -1,12 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 
-interface LoaderProps {}
-
-export const Loader: React.FC<LoaderProps> = ({}) => {
+export const ShowMessage: React.FC<{ message?: string }> = ({ message }) => {
   return (
     <Box mt="10%" ml="50%">
-      Loading...
+      {message || "Loading..."}
     </Box>
   );
 };
