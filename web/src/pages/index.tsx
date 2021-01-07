08b3c7fdb8e8fed48cbedf99f96fcfda6c3cf44b
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-08 00:17:05 +0530

    feat(web): add cursor pagination for posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,31 +1,73 @@
-import { Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
+import React, { useState } from "react";
 import { Layout } from "../components/Layout";
+import { ShowMessage } from "../components/ShowMessage";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
   });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+  console.log("data(landing): ", data);
+
+  if (!fetching && !data) {
+    return (
+      <ShowMessage message="You got no posts, query failed, please refresh and retry" />
+    );
+  }
 
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>Create a post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">Create a post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
-        <div>loading...</div>
+      {!data && fetching ? (
+        <ShowMessage />
       ) : (
-        <ul>
-          {data.posts.map((p) => (
-            <li key={p.id}>{p.title}</li>
+        <Stack spacing={8}>
+          {data!.posts.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
           ))}
-        </ul>
+        </Stack>
+      )}
+      {data && data.posts.hasMore && (
+        <Flex>
+          <Button
+            onClick={() =>
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+              })
+            }
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
+            load more
+          </Button>
+        </Flex>
       )}
     </Layout>
   );
