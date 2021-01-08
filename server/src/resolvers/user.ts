10345ac26b4e88beb9aecff24b9d2e4a600410b4
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-08 22:19:05 +0530

    feat(server): prevent current user from seeing other's email

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -3,10 +3,12 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  Root
 } from "type-graphql";
 import { getConnection } from "typeorm";
 import { v4 } from "uuid";
@@ -35,8 +37,18 @@ class UserResponse {
   user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver()
+  email(@Root() user: User, @Ctx() { req }: MyContext) {
+    // this is the current user and it is ok to show their own email
+    if (req.session.userId === user.id) {
+      return user.email;
+    }
+    // current user wants to see someone elses email
+    return "";
+  }
+
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg("token") token: string,
