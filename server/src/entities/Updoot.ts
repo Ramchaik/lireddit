337774b46d26d1025fa17db82d7e5d6b5a04befb
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-08 23:45:23 +0530

    feat(server): add updoot entity

---
+++ server/src/entities/Updoot.ts
@@ -0,0 +1,46 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Post } from "./Post";
+import { User } from "./User";
+
+/**
+ * m to n
+ * many to many
+ * user <-> post
+ * user -> join table <- posts
+ * user -> updoot <- posts
+ */
+
+@ObjectType()
+@Entity()
+export class Updoot extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column({ type: "int" })
+  value: number;
+
+  @Field()
+  @Column()
+  userId: number;
+
+  @Field(() => User)
+  @ManyToOne(() => User, (user) => user.updoots)
+  user: User;
+
+  @Field()
+  @Column()
+  postId: number;
+
+  @Field(() => Post)
+  @ManyToOne(() => Post, (post) => post.updoots)
+  post: Post;
+}
