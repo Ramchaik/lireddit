e3305894e4d779a941fdd183167f1355ee9360bf
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-01-12 21:41:44 +0530

    refactor(web): add hook to get Int id for a component

---
+++ web/src/utils/useGetIntId.ts
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+
+export const useGetIntId = () => {
+  const router = useRouter();
+  const id = router.query?.id;
+  const intId = typeof id === "string" ? parseInt(id) : -1;
+  return intId;
+};
